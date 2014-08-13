92ff68c7bec780e151ef0b168fd6707a8ed0977c
Rakesh Das <rakeshdaskowshik@gmail.com>
2014-08-12 20:58:42 -0500

    Initial Commit
    The complete app

---
+++ WindowsFormsApplication2/Calc GUI.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class CalcGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtInput1 = new System.Windows.Forms.TextBox();
+            this.txtInput2 = new System.Windows.Forms.TextBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.lblAns = new System.Windows.Forms.Label();
+            this.cbxOp = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtInput1
+            // 
+            this.txtInput1.Location = new System.Drawing.Point(85, 49);
+            this.txtInput1.Name = "txtInput1";
+            this.txtInput1.Size = new System.Drawing.Size(100, 22);
+            this.txtInput1.TabIndex = 0;
+            this.txtInput1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtInput1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInput1_KeyPress);
+            // 
+            // txtInput2
+            // 
+            this.txtInput2.Location = new System.Drawing.Point(85, 105);
+            this.txtInput2.Name = "txtInput2";
+            this.txtInput2.Size = new System.Drawing.Size(100, 22);
+            this.txtInput2.TabIndex = 1;
+            this.txtInput2.TextChanged += new System.EventHandler(this.txtInput2_TextChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(6, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(261, 25);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 19);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(86, 151);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(99, 23);
+            this.btnCalc.TabIndex = 5;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // lblAns
+            // 
+            this.lblAns.AutoSize = true;
+            this.lblAns.Location = new System.Drawing.Point(85, 194);
+            this.lblAns.Name = "lblAns";
+            this.lblAns.Size = new System.Drawing.Size(0, 13);
+            this.lblAns.TabIndex = 7;
+            // 
+            // cbxOp
+            // 
+            this.cbxOp.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbxOp.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cbxOp.FormattingEnabled = true;
+            this.cbxOp.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cbxOp.Location = new System.Drawing.Point(118, 78);
+            this.cbxOp.Name = "cbxOp";
+            this.cbxOp.Size = new System.Drawing.Size(35, 21);
+            this.cbxOp.TabIndex = 8;
+            this.cbxOp.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // CalcGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 294);
+            this.Controls.Add(this.cbxOp);
+            this.Controls.Add(this.lblAns);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.txtInput2);
+            this.Controls.Add(this.txtInput1);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "CalcGUI";
+            this.Text = "Calcaulator";
+            this.Load += new System.EventHandler(this.CalcGUI_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput1;
+        private System.Windows.Forms.TextBox txtInput2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Label lblAns;
+        private System.Windows.Forms.ComboBox cbxOp;
+    }
+}
+
---
+++ WindowsFormsApplication2/Calc GUI.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class CalcGUI : Form
+    {
+        public CalcGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripDropDownButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtInput2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            int firstNum = int.Parse(txtInput1.Text);
+            int secNum = int.Parse(txtInput2.Text);
+            if( cbxOp.SelectedIndex==0)
+            {
+            int ans = firstNum + secNum;
+            lblAns.Text = " Your answer is " + ans.ToString();
+            }
+            if (cbxOp.SelectedIndex == 1)
+            {
+                int ans = firstNum - secNum;
+                lblAns.Text = " Your answer is " + ans.ToString();
+            }
+            if (cbxOp.SelectedIndex == 2)
+            {
+                int ans = firstNum * secNum;
+                lblAns.Text = " Your answer is " + ans.ToString();
+            }
+            if (cbxOp.SelectedIndex == 3)
+            {
+                int ans = firstNum / secNum;
+                lblAns.Text = " Your answer is " + ans.ToString();
+            }
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalcGUI_Load(object sender, EventArgs e)
+        {
+            cbxOp.SelectedIndex = 0;
+        }
+
+        private void txtInput1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsNumber(e.KeyChar))
+            {
+            }
+            else
+            {
+                e.Handled = e.KeyChar != (char)Keys.Back;
+            }
+        }
+    }
+}
